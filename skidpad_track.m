79da77018d35eaceb070d2d6fafdbb12076a2b25
dbrisi <dbrisi@users.noreply.github.com>
2020-10-25 16:24:33 -0500

    Updating to make distance constant
    No true 0,0 point which may be a concern.

--- skidpad_track.m
+++ skidpad_track.m
@@ -2,35 +2,8 @@
 %% Skidpad Event XY Coordinates
 % Columbia FSAE
 
-% Create initial between points for straight line
+% Create initial between points for circle
 interval = 2*pi/360;
-
-% Length of the straight for the start of the event
-% NOTE: timing starts after the straight section is complete
-straight_start = (21.25/2) + 3; %3 meters is an estimation
-
-% Create start point for array (will be flipped later so we take max
-% length)
-y(1,1) = straight_start;
-j = 2; % counter
-
-% Loop to continually create data points increasing by interval
-while (straight_start - j*interval) >= 0
-    
-    y(j,1) = straight_start - j*interval;
-    
-    j = j + 1;
-    
-end 
-
-% Add start point or origin
-y(j,1) = 0;
-
-% Flip the array I build (recall it started at the end)
-y = flipud(y);
-% We only move in y direction, so make a vector of zeroes
-x = zeros(length(y),1);
-m = [x, y];
 
 % Create circles to reflect rest of course
 
@@ -48,6 +21,36 @@ right_laps = [right_laps(2:length(right_laps),:); right_laps(2:length(right_laps
 % Make laps 3 and 4
 left_laps=r*[cos(theta')-C(1) sin(theta')+C(2)];% the points you asked
 left_laps = [left_laps(2:length(left_laps),:); left_laps(2:length(left_laps),:)];
+
+% Length of the straight for the start of the event
+% NOTE: timing starts after the straight section is complete
+straight_start = (21.25/2) + 3; %3 meters is an estimation
+
+% Create start point for array (will be flipped later so we take max
+% length)
+y(1,1) = straight_start;
+j = 1; % counter
+
+% Calculate total distance between points based on circle
+distance = sqrt((right_laps(2,1) - right_laps(1,1))^2 + (right_laps(2,2) - right_laps(1,2))^2);
+
+% Loop to continually create data points increasing by interval
+while (straight_start - j*distance) >= 0
+    
+    y(j+1,1) = straight_start - j*distance;
+    
+    j = j + 1;
+    
+end 
+
+% Add start point or origin
+% DELETING FOR NOW
+
+% Flip the array I build (recall it started at the end)
+y = flipud(y);
+% We only move in y direction, so make a vector of zeroes
+x = zeros(length(y),1);
+m = [x, y];
 
 % Shift circles
 right_laps(:,2) = right_laps(:,2) + m(length(m),2);
