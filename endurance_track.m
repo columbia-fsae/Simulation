d2ccb5cd9b6b3861d4aa78bf3f23d8477f19b1cc
dbrisi <dbrisi@users.noreply.github.com>
2020-11-22 15:33:55 -0600

    Add files via upload

--- endurance_track.m
+++ endurance_track.m
@@ -1,6 +1,6 @@
 %% Read in data
 
-endurance_track_points = readmatrix('lat_long_endurance.csv')
+endurance_track_points = readmatrix('lat_long_endurance.csv');
 
 %% Transform the data
 
@@ -16,7 +16,7 @@ Y = endurance_track_points(:,1)*pi*6371*1000/180;
 
 Y = Y(:,1) - Y(1,1);
 
-endurance_track = [X,Y]
+endurance_track = [X,Y];
 
 
 
